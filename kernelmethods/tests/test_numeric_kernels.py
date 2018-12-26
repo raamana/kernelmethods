e13c0a64b012e8029a297c5750087f70c4afde64
Pradeep Reddy Raamana <raamana@gmail.com>
2018-12-26 12:18:37 -0500

    testing the properties of polynomial kernel for various param ranges

--- kernelmethods/tests/test_numeric_kernels.py
+++ kernelmethods/tests/test_numeric_kernels.py
@@ -44,3 +44,31 @@ def test_kernel_design(sample_dim):
 
         if not isinstance(result, Number):
             raise ValueError('result from {} is not a number!'.format(kernel.name))
+
+
+@hyp_settings(max_examples=1000)
+@given(strategies.integers(range_feature_dim[0], range_feature_dim[1]),
+       strategies.integers(range_polynomial_degree[0], range_polynomial_degree[1]),
+       strategies.floats(min_value=0, max_value=np.Inf,
+                         allow_nan=False, allow_infinity=False))
+def test_polynomial_kernel(sample_dim, poly_degree, poly_intercept):
+    """Tests specific for Polynomial kernel."""
+
+    # TODO input sparse arrays for test
+    x = gen_random_array(sample_dim)
+    y = gen_random_array(sample_dim)
+    poly = PolyKernel(degree=poly_degree, b=poly_intercept)
+
+    try:
+        result = poly(x, y)
+    except RuntimeWarning:
+        raise RuntimeWarning('RunTime warning for:\n'
+                             ' x={}\n y={}\n kernel={}\n'.format(x, y, poly))
+    except Exception:
+        raise Exception('unanticipated exception:\n'
+                        ' x={}\n y={}\n kernel={}\n'.format(x, y, poly))
+
+    if not isinstance(result, Number):
+        raise ValueError('poly kernel result {} is not a number!\n'
+                         'x={}\ny={}\nkernel={}\n'
+                         ''.format(result, x, y, poly))
