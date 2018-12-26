98671777047bf39f26c4a82268bfb114d3027993
Pradeep Reddy Raamana <raamana@gmail.com>
2018-12-26 12:29:05 -0500

    documenting what's offered as part of package

--- kernelmethods/__init__.py
+++ kernelmethods/__init__.py
@@ -2,6 +2,10 @@
 
 """Top-level package for kernelmethods."""
 
+__all__ = ['PolyKernel', 'BaseKernelFunction']
+
+from kernelmethods.base import BaseKernelFunction
+from kernelmethods.numeric_kernels import PolyKernel
 
 from ._version import get_versions
 __version__ = get_versions()['version']
