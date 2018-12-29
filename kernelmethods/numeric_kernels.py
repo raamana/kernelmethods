7ca07df6c4f976c201d6fac1c733abdc4ce4d604
Pradeep Reddy Raamana <raamana@gmail.com>
2018-12-28 22:53:56 -0500

    addition of ever green gaussian kernel

--- kernelmethods/numeric_kernels.py
+++ kernelmethods/numeric_kernels.py
@@ -54,3 +54,49 @@ class PolyKernel(BaseKernelFunction):
     # aliasing them to __str__ for now
     __format__ = __str__
     __repr__ = __str__
+
+
+class GaussianKernel(BaseKernelFunction):
+    """Polynomial kernel function"""
+
+    def __init__(self, sigma=2.0, skip_input_checks=False):
+        """
+        Constructor
+
+        Parameters
+        ----------
+        sigma : float
+            bandwidth
+
+        skip_input_checks : bool
+            Flag to skip input validation to save time.
+            Skipping validation is strongly discouraged for normal use,
+            unless you know exactly what you are doing (expert users).
+
+        """
+
+        super().__init__(name='gaussian')
+
+        # TODO implement param check
+        # ensuring values of gamma/gamma is eps or larger to avoid zero division
+        self.sigma = _ensure_min_eps(sigma)
+        self.gamma = _ensure_min_eps(1.0/(2*self.sigma**2))
+
+        self.skip_input_checks = skip_input_checks
+
+    def __call__(self, x, y):
+        """Actual implementation of kernel func"""
+
+        if not self.skip_input_checks:
+            x, y = check_input_arrays(x, y, ensure_dtype=np.number)
+
+        return np.exp(-self.gamma * np.linalg.norm(x - y, ord=2)**2)
+
+    def __str__(self):
+        """human readable repr"""
+
+        return "{}(sigma={})".format(self.name, self.sigma)
+
+    # aliasing them to __str__ for now
+    __format__ = __str__
+    __repr__ = __str__
