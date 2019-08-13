b8b82dce47dac594dadfdfad38a9765fef331f4c
Pradeep Reddy Raamana <raamana@gmail.com>
2019-08-13 18:13:44 -0400

    bug in light param ranges for bucket [skip ci]

--- kernelmethods/config.py
+++ kernelmethods/config.py
@@ -39,9 +39,9 @@ default_sigma_values_gaussian_kernel = tuple([2**exp for exp in range(-5, 6, 2)]
 default_gamma_values_laplacian_kernel = tuple([2**exp for exp in range(1, 7, 2)])
 
 # light
-light_degree_values_poly_kernel = (2, 3, 4)
-light_sigma_values_gaussian_kernel = tuple([2**exp for exp in range(-5, 6, 2)])
-light_gamma_values_laplacian_kernel = tuple([2**exp for exp in range(1, 7, 2)])
+light_degree_values_poly_kernel = (2, 3, )
+light_sigma_values_gaussian_kernel = tuple([2**exp for exp in range(-3, 3, 2)])
+light_gamma_values_laplacian_kernel = tuple([2**exp for exp in range(1, 3, 2)])
 
 # controls the precision for kernel_matrix elements
 km_dtype = np.dtype('f8')
