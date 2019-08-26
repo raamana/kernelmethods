9a3365e1969434b756a5ddd621643592c2e62d45
Pradeep Reddy Raamana <raamana@gmail.com>
2019-08-26 19:03:32 -0400

    updating import setup

--- kernelmethods/__init__.py
+++ kernelmethods/__init__.py
@@ -3,15 +3,19 @@
 """Top-level package for kernelmethods."""
 
 __all__ = ['KernelMatrix',
-           'BaseKernelFunction', 'KernelMethodsException', 'KMAccessError',
+           'BaseKernelFunction',
+           'KernelMethodsException', 'KMAccessError', 'KMNormError',
+           'KMSetAdditionError',
            'PolyKernel', 'GaussianKernel', 'LaplacianKernel', 'LinearKernel',
-           'KernelBucket', 'KernelSet']
+           'Chi2Kernel', 'SigmoidKernel',
+           'KernelBucket', 'KernelSet',
+           'OptimalKernelSVR', 'KernelMachine']
 
 from kernelmethods.config import KernelMethodsException, \
     KMAccessError, KMNormError, KMSetAdditionError
 from kernelmethods.base import BaseKernelFunction, KernelMatrix, KernelSet
 from kernelmethods.numeric_kernels import PolyKernel, GaussianKernel, LaplacianKernel, \
-    LinearKernel
+    LinearKernel, Chi2Kernel, SigmoidKernel
 from kernelmethods.sampling import KernelBucket
 from kernelmethods.algorithms import KernelMachine, OptimalKernelSVR
 
