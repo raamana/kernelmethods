4134406d6410d1036d58903136dc875ed34d5989
Pradeep Reddy Raamana <raamana@gmail.com>
2019-08-27 22:51:47 -0400

    opt kernel SVR

--- kernelmethods/algorithms.py
+++ kernelmethods/algorithms.py
@@ -474,7 +474,8 @@ class OptimalKernelSVR(BaseOptimalKernelMachine, SVR):
     """
 
 
-    def __init__(self, k_bucket='exhaustive',
+    def __init__(self,
+                 k_bucket='exhaustive',
                  method='cv_risk',
                  C=1.0,
                  epsilon=0.1,
@@ -505,17 +506,12 @@ class OptimalKernelSVR(BaseOptimalKernelMachine, SVR):
 
         tol : float, optional (default=1e-3)
             Tolerance for stopping criterion.
-    @abstractmethod
-    def _find_optimal_kernel(self):
-        """Method to find the optimal kernel
 
-        Given a kernel bucket, a training sample and a ranking method. To be
-        defined by the child class, appropriate for their task i.e. classification
-        or regression
         """
 
-        super().__init__(kernel='precomputed', C=C, epsilon=epsilon,
-                         shrinking=shrinking, tol=tol)
+        # not init'ing SVC/SVR with kernel='precomputed' to avoid issues with
+        # cross_val_score and safe_split
+        super().__init__(C=C, epsilon=epsilon, shrinking=shrinking, tol=tol)
 
         self.k_bucket = k_bucket
         self.method = method
@@ -525,101 +521,15 @@ class OptimalKernelSVR(BaseOptimalKernelMachine, SVR):
         self.tol = tol
 
 
-    def fit(self, X, y, sample_weight=None):
-        """Estimate the optimal kernel, and fit a SVM based on the custom kernel.
+    def _find_optimal_kernel(self):
+        """Method to find the optimal kernel"""
 
-        Parameters
-        ----------
-        X : {array-like, sparse matrix}, shape (n_samples, n_features)
-            Training vectors, where n_samples is the number of samples
-            and n_features is the number of features.
-            For kernel="precomputed", the expected shape of X is
-            (n_samples, n_samples).
+        self._opt_ker_search_est_name = 'SVR'
 
-        y : array-like, shape (n_samples,)
-            Target values (class labels in classification, real numbers in
-            regression)
-
-        sample_weight : array-like, shape (n_samples,)
-            Per-sample weights. Rescale C per sample. Higher weights
-            force the classifier to put more emphasis on these points.
-
-        Returns
-        -------
-        self : object
-
-        Notes
-        ------
-        If X and y are not C-ordered and contiguous arrays of np.float64 and
-        X is not a scipy.sparse.csr_matrix, X and/or y may be copied.
-
-        If X is a dense array, then the other methods will not support sparse
-        matrices as input.
-
-        """
-
-        if isinstance(self.k_bucket, str):
-            try:
-                # using a new internal variable to retain user supplied param
-                self._k_bucket = make_kernel_bucket(self.k_bucket)
-            except:
-                raise ValueError('Input for k_func can only an instance of '
-                                 'KernelBucket or a sampling strategy to generate '
-                                 'one with make_kernel_bucket.'
-                                 'sampling strategy must be one of {}'
-                                 ''.format(cfg.kernel_bucket_strategies))
-        elif isinstance(self.k_bucket, KernelBucket):
-            self._k_bucket = deepcopy(self.k_bucket)
-        else:
-            raise ValueError('Input for k_func can only an instance of '
-                             'KernelBucket or a sampling strategy to generate '
-                             'one with make_kernel_bucket')
-
-        self._train_X, self._train_y = check_X_y(X, y, y_numeric=True)
-
-        self.opt_kernel_ = self._find_optimal_kernel()
-
-        super().fit(X=self.opt_kernel_.full, y=self._train_y,
-                    sample_weight=sample_weight)
-
-        # temporary hack to pass sklearn estimator checks till a bug is fixed
-        # for more see: https://github.com/scikit-learn/scikit-learn/issues/14712
-        self.n_iter_ = 1
-
-        return self
-
-
-    def predict(self, X):
-        """
-        Perform classification on samples in X.
-
-        For an one-class model, +1 or -1 is returned.
-
-        Parameters
-        ----------
-        X : {array-like, sparse matrix}, shape (n_samples, n_features)
-            For kernel="precomputed", the expected shape of X is
-            [n_samples_test, n_samples_train]
-
-        Returns
-        -------
-        y_pred : array, shape (n_samples,)
-            Class labels for samples in X.
-        """
-
-        if not hasattr(self, 'opt_kernel_'):
-            raise ValueError("Can't predict - not fitted yet! Run .fit() first.")
-
-        X = check_array(X)
-
-        # sample_one must be test data to get the right shape for sklearn X
-        self.opt_kernel_.attach_to(sample_one=X, sample_two=self._train_X)
-        test_train_KM = self.opt_kernel_.full
-        predicted_y = super().predict(test_train_KM)
-
-        return predicted_y
-        # TODO we don't need data type coversion, as its not classification?
-        # return np.asarray(predicted_y, dtype=np.intp)
+        return find_optimal_kernel(self._k_bucket,
+                                   self._train_X, self._train_y,
+                                   method=self.method,
+                                   estimator_name=self._opt_ker_search_est_name)
 
 
     def get_params(self, deep=True):
